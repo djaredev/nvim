92edd9b48d7edff868677bcafef4d21670f52100
Diego Jared <djjaredev@gmail.com>
2024-07-19 16:54:18 -0600

    🔄 Update keymaps

--- lua/djaredev/config/keymaps.lua
+++ lua/djaredev/config/keymaps.lua
@@ -1,7 +1,6 @@
 local wk = require("which-key")
 
 -- Move Lines
-
 vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
 vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
 vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
@@ -9,21 +8,41 @@ vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
 vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down",  silent=true}) -- slient = true : mapped command will not be shown on the command line
 vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up",  silent=true})
 
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Buffer
+wk.add({"<leader>b", group = "Buffer"})
+vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+vim.keymap.set("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+-- vim.keymap.set("n", "<leader>bd", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
+vim.keymap.set("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
+
 -- Telescope 
-vim.keymap.set("n", "<leader>fg", function() require('telescope.builtin').git_files({ show_untracked = true }) end, {desc = "Telescope Git Files"})
-vim.keymap.set("n", "<leader>fb", function() require("telescope.builtin").buffers() end, {desc = "Telescope buffers"})
-vim.keymap.set("n", "<leader>gs", function() require("telescope.builtin").git_status().buffers() end, {desc = "Telescope Git status"})
-vim.keymap.set("n", "<leader>gc", function() require("telescope.builtin").git_bcommits() end, {desc = "Telescope Git commits"})
-vim.keymap.set("n", "<leader>gb", function() require("telescope.builtin").git_branches() end, {desc = "Telescope Git branches"})
-vim.keymap.set("n", "<leader>ff", function() require('telescope.builtin').find_files() end, {desc = "Telescope Find Files"})
-vim.keymap.set("n", "<leader>fh", function() require("telescope.builtin").help_tags() end, {desc = "Telescope Help"})
-vim.keymap.set("n", "<leader>fs", function() require("telescope").extensions.file_browser.file_browser({ path = "%:h:p", select_buffer = true }) end, {desc = "Telescope file browser"})
---vim.keymap.set("n", "<leader>pe", function() require("telescope.builtin").buffers() end, {desc = "Telescope buffers"})
-wk.register({f = { name = "Files" }, }, {prefix = "<leader>"})
-wk.register({g = { name = "Git Files" }, }, {prefix = "<leader>"})
+wk.add({"<leader>f", group = "Find/Search"})
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", {desc = "Show open buffers"})
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", {desc = "Find files"})
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", {desc = "Help tags"})
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", {desc = "File browser"})
+vim.keymap.set("n", "<leader>fl", "<cmd>Telescope live_grep<cr>", {desc = "Grep on current working directory"})
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope grep_string<cr>", {desc = "Grep under cursor"})
+vim.keymap.set("n", "<leader>fc", "<cmd>Telescope colorscheme<cr>", {desc = "Lists available colorschemes"})
+
+
+
+wk.add({"<leader>g", group = "Git"})
+vim.keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>", {desc = "Git status"})
+vim.keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", {desc = "Git commits"})
+vim.keymap.set("n", "<leader>gb", "<cmd>Telescope git_braches<cr>", {desc = "Git branches"})
 
 
 -- Trouble
+wk.add({"<leader>t", group = "Trouble"})
 vim.keymap.set("n", "<leader>tx", function() require("trouble").toggle() end, {desc = "Open Trouble"})
 vim.keymap.set("n", "<leader>tw", function() require("trouble").toggle("diagnostics") end, {desc = "Workspace diagnostics"})
 vim.keymap.set("n", "<leader>td", function() require("trouble").toggle("diagnostics") end, {desc = "Document diagnostics"})
@@ -31,9 +50,9 @@ vim.keymap.set("n", "<leader>tq", function() require("trouble").toggle("qflist")
 vim.keymap.set("n", "<leader>tl", function() require("trouble").toggle("loclist") end, {desc = "Location list"})
 vim.keymap.set("n", "<leader>tc", function() require("trouble").close() end, {desc = "Close Trouble"})
 vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp") end, {desc = "Lsp references"})
-wk.register({t = { name = "Trouble" }, }, {prefix = "<leader>"})
 
 -- Debug
+wk.add({"<leader>d", group = "Debugger"})
 vim.keymap.set("n", "<leader>db", function() require('dap').toggle_breakpoint() end, {desc = "Breakpoint"})
 vim.keymap.set("n", "<leader>dc", function() require('dap').continue() end, {desc = "Continue"})
 vim.keymap.set("n", "<leader>do", function() require('dap').step_over() end, {desc = "Step Over"})
@@ -41,16 +60,15 @@ vim.keymap.set("n", "<leader>di", function() require('dap').step_into() end, {de
 vim.keymap.set("n", "<leader>dO", function() require('dap').step_out() end, {desc = "Step Out"})
 vim.keymap.set("n", "<leader>dr", function() require('dap').repl.open() end, {desc = "Repl Open"})
 vim.keymap.set("n", "<leader>dx", function() require('dap').terminate() end, {desc = "Exit"})
-wk.register({d = { name = "Debug" }, }, {prefix = "<leader>"})
 
 
 -- Safe file
-vim.keymap.set("n", "<C-s>", ":w <CR>", {desc = "Save file", silent = true})
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<CMD>w<CR>", {desc = "Save file", silent = true})
 
 -- Code run
+wk.add({"<leader>x", group = "Code options"})
 vim.keymap.set("n", "<leader>xp", ":TermExec cmd='python %'<CR>", {desc = "Execute Python file", silent = true})
 vim.keymap.set("n", "<leader>xf", function() require("conform").format({ timeout_ms = 500, lsp_fallback = true}) end, {desc = "Format code", silent = true})
-wk.register({x = { name = "Code options" }, }, {prefix = "<leader>"})
 
 -- Oil 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
