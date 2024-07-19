efe8dc6ce7d283a4239ed8d79eea9d8f4a4789c3
Diego Jared <djjaredev@gmail.com>
2024-07-19 17:31:08 -0600

    🔴 Update plugins

--- lua/djaredev/plugins/treesitter.lua
+++ lua/djaredev/plugins/treesitter.lua
@@ -1,23 +1,31 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = "VeryLazy",
-    main = "nvim-treesitter.configs",
-    opts = {
-        highlight = { enable = true },
-        indent = { enable = true },
-        autopairs = { enable = true},
-        ensure_installed = {
-            "python",
-            "rust",
-            "lua",
-            "luadoc",
-            "bash",
-            "cpp",
-            "c",
-            "csv",
-            "vim",
-            "yaml",
+    {
+        "nvim-treesitter/nvim-treesitter", 
+        build = ":TSUpdate",
+        event = "VeryLazy",
+        main = "nvim-treesitter.configs",
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            autopairs = { enable = true},
+            ensure_installed = {
+                "python",
+                "rust",
+                "lua",
+                "luadoc",
+                "bash",
+                "cpp",
+                "c",
+                "csv",
+                "vim",
+                "yaml",
+            },
         },
     },
+    {
+        "nvim-treesitter/nvim-treesitter-context"
+    }
 }
+
+        
+    
