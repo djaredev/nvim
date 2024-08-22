4e67b71c9e22ac0bdbfc9e2531b34dba226a45b9
Diego Jared <djjaredev@gmail.com>
2024-08-21 20:07:30 -0600

    🔧 Update: Disable search highlighting

--- lua/djaredev/config/options.lua
+++ lua/djaredev/config/options.lua
@@ -5,3 +5,4 @@ vim.opt.shiftwidth           = 4
 vim.g.mapleader              = " "
 vim.opt.termguicolors        = true -- 24-bit colour
 vim.opt.clipboard            = 'unnamedplus'
+vim.opt.hlsearch             = false
