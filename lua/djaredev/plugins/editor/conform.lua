8bcca03629eb5203563983afd81998f906099191
Diego Jared <djjaredev@gmail.com>
2024-10-13 15:23:35 -0600

    🔧 Update lazy loading in conform.nvim

--- lua/djaredev/plugins/editor/conform.lua
+++ lua/djaredev/plugins/editor/conform.lua
@@ -1,6 +1,6 @@
 return {
     'stevearc/conform.nvim',
-    ft = { "python" },
+    ft = { "python", "javascript", "html", "css" },
     -- event = { "BufReadPre", "BufNewFile" },
     config = function()
         require("conform").setup({
