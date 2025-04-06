1ab436e116e802626e756649c94a1e61db1c3a3a
Diego Jared <djjaredev@gmail.com>
2025-04-06 13:16:13 -0600

    💄 Update catppuccin colorscheme

--- lua/djaredev/plugins/colorscheme.lua
+++ lua/djaredev/plugins/colorscheme.lua
@@ -21,67 +21,23 @@ return {
 		lazy = false,
 		priority = 1000,
 		opts = {
-
 		},
 		config = function()
-			require("catppuccin").setup {
+			require("catppuccin").setup({
+				transparent_background = true,
 				color_overrides = {
 					mocha = {
 						base = "#000000",
 						mantle = "#000000",
 						crust = "#000000",
 					}
-				}
-			}
-
-			-- load the colorscheme here
-			vim.cmd([[colorscheme catppuccin-mocha]])
-
-			-- require("catppuccin").setup {
-			--     integration = {
-			--       telescope = { enabled = true, border = false },
-			--       cmp = { enabled = true, border = false },
-			--       which_key = { enabled = true, border = false }
-			--     }
-			-- }
-
-			-- integration = {
-			--     telescope = { enabled = true, border = false },
-			--     cmp = { enabled = true, border = false },
-			--     which_key = { enabled = true, border = false }
-			-- }
-			-- cmp = {
-			--     enabled = true,
-			--     border = {
-			--       completion = true,
-			--       documentation = true
-			--     }
-			-- }
-			-- telescope = { enabled = true, border = false }
+				},
+				integrations = {
+					blink_cmp = true,
+				},
+			})
+			vim.cmd.colorscheme "catppuccin-mocha"
 		end,
 
-		integrations = {
-			telescope = { enabled = false, border = false },
-			cmp = { enabled = true, border = false },
-			-- which_key = { enabled = true, border = false }
-			native_lsp = {
-				enabled = false,
-				virtual_text = {
-					errors = { "italic" },
-					hints = { "italic" },
-					warnings = { "italic" },
-					information = { "italic" },
-				},
-				underlines = {
-					errors = { "underline" },
-					hints = { "underline" },
-					warnings = { "underline" },
-					information = { "underline" },
-				},
-				inlay_hints = {
-					background = false,
-				},
-			},
-		}
 	},
 }
