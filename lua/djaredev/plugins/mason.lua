1ff9182ffe22fe28bb6d0ac94f45fbb14e997b70
Diego Jared <djjaredev@gmail.com>
2025-08-10 19:27:02 -0600

    🔥 Remove mason-lspconfig plugin

--- lua/djaredev/plugins/mason.lua
+++ lua/djaredev/plugins/mason.lua
@@ -1,21 +1,4 @@
 return {
 	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	},
-	-- cmd = "Mason",
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"pyright",
-				"ts_ls",
-				"jdtls",
-				"html",
-				"cssls",
-				"lua_ls",
-			}
-		})
-	end,
+	opts = {}
 }
