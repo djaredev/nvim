300abadb60f125e53e416540d043185d63142dd1
Diego Jared <djjaredev@gmail.com>
2025-10-10 14:26:07 -0600

    🔥 Disable paste with OSC 52

--- lua/djaredev/config/options.lua
+++ lua/djaredev/config/options.lua
@@ -4,7 +4,28 @@ vim.opt.tabstop        = 4
 vim.opt.shiftwidth     = 4
 vim.opt.expandtab      = true
 vim.g.mapleader        = " "
-vim.g.clipboard = "osc52" -- force OSC52
 vim.opt.termguicolors  = true -- 24-bit colour
 vim.opt.clipboard      = 'unnamedplus'
 -- vim.opt.hlsearch             = false
+
+-- To delete the message "clipboard: provider returned invalid data"
+-- See: https://github.com/neovim/neovim/discussions/28010#discussioncomment-9877494
+local function disable_paste()
+    return {
+        vim.fn.split(vim.fn.getreg(""), "\n"),
+        vim.fn.getregtype(""),
+    }
+end
+
+-- Enable only copy
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = disable_paste,
+    ['*'] = disable_paste,
+  },
+}
